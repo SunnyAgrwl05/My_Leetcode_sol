57cde99646fba887a7810b785d88e15af4efba85
Sunny Kumar <136143741+SunnyAgrwl05@users.noreply.github.com>
2026-06-19 23:34:01 +0530

    Time: 0 ms (100%), Space: 10.9 MB (44.68%) - LeetHub

---
+++ 1732-find-the-highest-altitude/1732-find-the-highest-altitude.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public: // code written by sunny
+    int largestAltitude(vector<int>& gain) {
+        int curr = 0, maxAlt = 0;
+        
+        for (int g : gain) {
+            curr += g;
+            maxAlt = max(maxAlt, curr);
+        }
+        
+        return maxAlt;
+    }
+};
